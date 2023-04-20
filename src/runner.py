a41228aa9759f30185bb1614ae90af4159e4bfe7
John Borton <83604853+Deistal@users.noreply.github.com>
2023-04-20 13:12:58 -0400

    Add files via upload

--- src/runner.py
+++ src/runner.py
@@ -1,19 +1,22 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QTableWidgetItem
+from employeeController import EmployeeMainController
 from employerController import EmployerMainController
 import sys
 
 class Controller:
-    def __init__(self):
+    def __init__(self, type='employer'):
         self.app = QtWidgets.QApplication(sys.argv)
-        self.view = EmployerMainController()
-
+        if(type=='employer'):
+            self.view = EmployerMainController()
+        elif(type=='employee'):
+            self.view = EmployeeMainController()
+        else:
+            return
+    
     def run(self):
         self.view.show()
         return self.app.exec_()
-
-    
-    
 
 if __name__ == '__main__':
     c = Controller()
