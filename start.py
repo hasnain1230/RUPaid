81f0b9c01d8e5e2190f86b60cf1e556afa6d7915
Hasnain <hasnain1230@gmail.com>
2023-04-30 02:44:34 -0400

    Fixed so now that it'll run on any system

--- start.py
+++ start.py
@@ -2,24 +2,23 @@ import os
 import platform
 import subprocess
 
+
 if __name__ == '__main__':
     os_name = platform.system()
 
-    if os_name == 'Windows':
-        # Modify PYTHONPATH in the current environment
-        os.environ['PYTHONPATH'] = f"{os.environ.get('PYTHONPATH', '')};{os.getcwd()}"
+    # Modify PYTHONPATH in the current environment
+    path_separator = ';' if os_name == 'Windows' else ':'
+    os.environ['PYTHONPATH'] = f"{os.environ.get('PYTHONPATH', '')}{path_separator}{os.getcwd()}"
 
-        # Change the current working directory to src
-        os.chdir('src')
+    # Print PYTHONPATH
+    print(f"PYTHONPATH: {os.environ.get('PYTHONPATH')}")
 
-        # Run the Python script in the modified environment
-        subprocess.run(['python', f"RUPaid{os.sep}runner.py"], env=os.environ)
+    # Change the current working directory to src
+    os.chdir('src')
 
-        # Change back to the previous working directory
-        os.chdir('..')
-    else:
-        os.system(f"export PYTHONPATH=$PYTHONPATH:{os.getcwd()}")
-        print(f"PYTHONPATH: {os.environ.get('PYTHONPATH')}")
-        os.chdir("src")
-        os.system(f"python RUPaid{os.sep}runner.py")
-        os.chdir("..")
+    # Run the Python script in the modified environment
+    python_executable = 'python' if os_name == 'Windows' else 'python3'
+    subprocess.run([python_executable, f"RUPaid{os.sep}runner.py"], env=os.environ)
+
+    # Change back to the previous working directory
+    os.chdir('..')
